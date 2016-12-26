b928ddb00d8a3ade67ff7d8e028523edafd4a225
CrimsonGlory <javierbassi@gmail.com>
2016-12-26 16:51:16 -0300

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+## Installation
+First install [docker](https://www.docker.com) and [docker-compose](https://docs.docker.com/compose/), then:
+```
+cd codex-backend
+docker-compose up
+```
+
---
+++ codex-backend/src/config/default_config.py
@@ -0,0 +1,41 @@
+env = {
+    "metadata":
+    {
+      "host": "codexbackend_db_1",
+      "port": 27017
+      },
+    "files":
+    {
+      "host": "codexbackend_db_1",
+      "port": 27017
+      },
+    "temp_files":
+    {
+      "host": "codexbackend_db_2",
+      "port": 27017
+      },
+    "versions":
+    {
+      "host": "codexbackend_db_1",
+      "port": 27017
+      },
+    "temporal_files_db": False,
+    "db_temp_files_name": "DB_temp_files",
+    "db_files_name": "DB_files",
+    "db_versions_name": "DB_versions",
+    "db_metadata_name": "DB_metadata",
+    "db_metadata_collection": "meta_container",
+    "vt_apikey": "",
+    'yara-script1': '/myapp/yara/YaraGenerator/yaraGenerator.py',
+    'yara-script2': '/myapp/yara/yarGen-master/yarGen.py',
+    'csv-upx-file': '',
+    'auto_get_av_result': False,
+    "mailsender":
+    {
+        "fromaddr": "",
+        "smtp_host": "smtp.gmail.com",
+        "smtp_ssl_port": 465,
+        "username": '',
+        "password": ''
+        }
+    }
